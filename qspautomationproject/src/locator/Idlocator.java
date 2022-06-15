3b1acbec6368f922eda640656d44ce36ab47da7c
Komatwark <107527745+Komatwark@users.noreply.github.com>
2022-06-15 09:45:42 +0530

    Update Idlocator.java

--- qspautomationproject/src/locator/Idlocator.java
+++ qspautomationproject/src/locator/Idlocator.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Idlocator {
 
 	public static void main(String[] args) {
+		// set the path for driver
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
